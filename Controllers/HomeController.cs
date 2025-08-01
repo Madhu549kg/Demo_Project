8a7c9dcf8885f486d75a408f1f241d1c53e1fc9a
MadhusudanKG <kgmadhusudan8@gmail.com>
2025-08-01 11:14:00 +0530

    new comment added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Demo_Project.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your applicatccion description page.";
+            ViewBag.Message = "Your apxplicatccion description page.";
 
             return View();
         }
